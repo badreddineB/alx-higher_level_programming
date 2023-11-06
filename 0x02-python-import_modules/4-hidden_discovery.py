ff789dd910c411d6873ff01f4da6b65e1b4eb28e
badreddineB <bouaicha.badreddine18@gmail.com>
2023-11-06 13:37:21 -0800

    done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import hidden_4
-    for l in dir(hidden_4):
-        if l[0:2] != "__":
-            print(l)
+
+    names = dir(hidden_4)
+
+    for name in names:
+        if "__" not in name[:2]:
+            print("{:s}".format(name))
